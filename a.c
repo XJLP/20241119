2672579804f2ef82b832119a4c7fbbae1fce391e
XJLP <424669510@qq.com>
2025-11-28 18:34:18 +0800

    the 2 test

--- a.c
+++ a.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 int main()
 {
+	
+
 
 	printf("this is the first test.");
-	return 0;
+	printf("this is the first 2test.");
+	printf("this is the first 3test.");
+	printf("this is the first 4test.");
+	
 }
